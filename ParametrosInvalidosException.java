a340f8d10711128ddf46b09e283322fdfbee46fe
talita.Gama <talitagama0810@gmail.com>
2025-05-29 17:42:59 -0300

    commit final

---
+++ ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+public class ParametrosInvalidosException extends Exception {
+
+    // Construtor que recebe uma mensagem e a passa para a classe pai (Exception)
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
